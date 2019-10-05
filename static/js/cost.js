788387a2072c38109a72424e4817c0c24123b38c
hyeonji <hyeonji.27@daum.net>
2019-10-06 02:40:10 +0900

    costPage checkbox 오류 거의 다 해결

--- static/js/cost.js
+++ static/js/cost.js
@@ -1,9 +1,9 @@
-function addCost(i, count) {
+function addCost(i, count, c) {
     var tagList = '';
     var collapseExample = "collapseExample";
     var checkbox = "checkbox";
     var user = "user";
-
+    //count는 plan을 수정할 수 있는 사람들의 수
         collapseExample += i;
         checkbox += i;
         tagList += '<div class="row">';
@@ -16,11 +16,11 @@ function addCost(i, count) {
         tagList += '<button type="button" class="btn btn-white btn-round btn-just-icon" data-toggle="collapse" data-target="#'+collapseExample+'" aria-expanded="false" aria-controls="'+collapseExample+'">';
         tagList += '<i class="material-icons">'+"person_outline"+'</i></button>';
         tagList += '<div class="collapse" id="'+collapseExample+'" aria-expanded="true" style>';
-        
         tagList += '<div class="cdiv"><ul class="ks-cboxtags">';
-        tagList += '<li><input type="checkbox" id="'+checkbox+'" value="Rainbow Dash" checked><label for="'+checkbox+'">'+user+'</label></li>';
-        tagList += '<li><input type="checkbox" id="'+checkbox+'" value="Rainbow Dash" checked><label for="'+checkbox+'">'+user+'</label></li>';
-        tagList += '<li><input type="checkbox" id="'+checkbox+'" value="Rainbow Dash" checked><label for="'+checkbox+'">'+user+'</label></li>';
+        for(var j=0; j<count; j++) {
+            tagList += '<li><input type="checkbox" id="'+checkbox+c+'" value="Rainbow Dash" checked><label for="'+checkbox+c+'">'+user+'</label></li>';
+            c++;
+        }
         tagList += '</ul></div></div></div></div>';
 
         $('#costInputArea').append(tagList);
