9082f16116708e3bf49fbc5966c90e631fa1b7ae
hyunju <phj2309@naver.com>
2019-10-06 20:16:48 +0900

    뷰하는중하는중하는중

--- Router/Expense/controller.js
+++ Router/Expense/controller.js
@@ -2,5 +2,5 @@ const mapper = require('../../DB/mapperController.js');
 
 exports.t = async function(req, res)
 {
-	res.render("plan.html"); //expense.html이 없어서 일단 plan으로 연결
+	res.render("expense.html"); //expense.html이 없어서 일단 plan으로 연결
 }
